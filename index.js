8c93fb467d75521df93efd5559f615df9a0d4355
sumon2807 <sharifulbd911@gmail.com>
2023-12-31 00:01:36 +0600

    my first commit

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ const port=process.env.PORT || 5000;
 // middleware
 app.use(cors());
 
+const users=[
+    {id:1, name:'sumon', email:'sumon@gmail.com'},
+    {id:2, name:'moonmoon', email:'monmon@gmail.com'},
+    {id:3, name:'kajol', email:'kajol@gmail.com'}
+]
+
+app.get('/users', (req, res)=>{
+    res.send(users);
+})
+
 app.get('/', (req, res)=>{
     res.send('e-Shop is running...')
 });
